d76b00fad3e53cf9af9ee1b1f0b96c036edc324b
Bayu Wardana <wardanabayu455@gmail.com>
2022-07-12 14:22:58 +0900

    add file migration posts

---
+++ migrations/20220712051856_create_posts_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  knex.schema.createTable('posts', (table) => {
+    table.increments();
+    table.string('title');
+    table.text('content');
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  knex.schema.dropTable('posts');
+};
